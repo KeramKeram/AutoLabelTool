a60ff10adc7f7f9bf0bb4c022b3d9d93210a6f14
KeramKeram <rev@autograf.pl>
2019-09-24 21:46:12 +0200

    Updating configuration, Adding example files Adding code responsible for reading images and sending them to paste and then save output.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from PIL import Image
 import configparser
+import glob
+
+
 file_path = "configuration.ini"
 
 def paste_image(template_image, image, left, upper, right, lower):
@@ -11,9 +14,18 @@ def load_configuration_file(path):
     config.read(path)
     return config
 
-def get_configuration_data(input_data):
+def get_configuration_data():
     configuration = load_configuration_file(file_path)
     template = configuration['InputFiles']['Template']
     images_to_paste_dir = configuration['InputFiles']['ImagesToPasteDir']
-    return [template, images_to_paste_dir]
+    return template, images_to_paste_dir
 
+template, images_to_paste_dir = get_configuration_data()
+
+template_image = Image.open(template)
+
+for filename in glob.glob(images_to_paste_dir + '/*.jpg'):
+    im=Image.open(filename)
+    width, height = im.size
+    output_image = paste_image(template_image, im, 0, 0 , width, height)
+    output_image.save('output.jpg')
